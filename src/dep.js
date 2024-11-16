95ab076a5dd25a558d3aa2db44f511dceb0fad3c
handoko <srabutdotcom@gmail.com>
2024-11-16 10:30:45 +0700

    add specifier

--- src/dep.js
+++ src/dep.js
@@ -1,2 +1,2 @@
-export { Struct, Constrained, Byte, Uint8, Uint16, Uint24, Uint32 } from "jsr:@tls/struct"
-export { ExtensionType } from "jsr:@tls/enum"
+export { Struct, Constrained, Byte, Uint8, Uint16, Uint24, Uint32 } from "jsr:@tls/struct@^0.2.2"
+export { ExtensionType } from "jsr:@tls/enum@^0.0.7"
